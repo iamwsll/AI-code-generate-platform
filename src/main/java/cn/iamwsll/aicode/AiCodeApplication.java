51e8fa068c29b9eeac38d429f4cdd17d1e12b147
iamwsll <1481121704@qq.com>
2025-10-26 15:42:31 +0800

    init item

---
+++ src/main/java/cn/iamwsll/aicode/AiCodeApplication.java
@@ -0,0 +1,13 @@
+package cn.iamwsll.aicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/iamwsll/aicode/controller/HealthController.java
@@ -0,0 +1,15 @@
+package cn.iamwsll.aicode.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthy")
+public class HealthController {
+
+    @GetMapping("")
+    public String healthCheck() {
+        return "I'm healthy!";
+    }
+}
