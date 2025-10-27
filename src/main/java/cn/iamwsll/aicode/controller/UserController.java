5882c7762c34ab55d753141d9a485d0844e9f324
iamwsll <1481121704@qq.com>
2025-10-27 22:55:33 +0800

    add user management page with search functionality and user deletion

--- src/main/java/cn/iamwsll/aicode/controller/UserController.java
+++ src/main/java/cn/iamwsll/aicode/controller/UserController.java
@@ -15,14 +15,11 @@ import cn.iamwsll.aicode.model.vo.UserVO;
 import com.mybatisflex.core.paginate.Page;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import cn.iamwsll.aicode.model.entity.User;
 import cn.iamwsll.aicode.service.UserService;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +33,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Resource
@@ -151,6 +149,7 @@ public class UserController {
         if (deleteRequest == null || deleteRequest.getId() <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
+        log.info("delete user id = {}", deleteRequest.getId());
         boolean b = userService.removeById(deleteRequest.getId());
         return ResultUtils.success(b);
     }
