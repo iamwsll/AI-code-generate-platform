d00b8f09f155fab2b8d7a1567ef4d61bdd2bc1e1
iamwsll <1481121704@qq.com>
2025-11-02 16:30:12 +0800

    add download functionality for application code with backend support

---
+++ src/main/java/cn/iamwsll/aicode/service/ProjectDownloadService.java
@@ -0,0 +1,13 @@
+package cn.iamwsll.aicode.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface ProjectDownloadService {
+    /**
+     * 下载项目为zip文件
+     * @param projectPath
+     * @param downloadFileName
+     * @param response
+     */
+    void downloadProjectAsZip(String projectPath, String downloadFileName, HttpServletResponse response);
+}
